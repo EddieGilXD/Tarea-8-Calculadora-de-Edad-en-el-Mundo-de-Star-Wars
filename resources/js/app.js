77f4b4d3f5b77733cb9a1bb4b59272d251a040b7
EddieGilXD <djuntaied@gmail.com>
2025-03-18 00:35:48 -0600

    Tarea 8 logica terminara aun que se puede modificar para agregar detalles y controlar el css

---
+++ resources/js/app.js
@@ -0,0 +1,16 @@
+function convertirEdad() {
+    let edadEspecie = parseInt(document.getElementById("especie").value);
+    let edad = document.getElementById("edad").value;    
+    console.log(edad);
+    console.log(edadEspecie);
+    
+    if (edad > 0) {
+        let edadEquivalente = (edad * edadEspecie) / 80;
+        console.log(edadEquivalente);
+        document.getElementById("respuesta").innerText = "La edad equivalente es de: " + edadEquivalente + " años";
+        return;
+    } else {
+        console.log("Error al ingresar edad intente de nuevo");
+        return;
+    }
+}
